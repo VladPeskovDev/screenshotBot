7ac2b3948afb833111355973b82c45b56f1fcac3
VladPeskovDev <vladpeskovdev@gmail.com>
2025-07-21 18:37:33 +0300

    chore:readme

--- electron-app/overlay-preload.js
+++ electron-app/overlay-preload.js
@@ -3,22 +3,18 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('overlayBridge', {
   
   // Подписаться на обновление текста в оверлее
-   
   onUpdateText: (callback) =>
     ipcRenderer.on('update-overlay-text', (_, text) => callback(text)),
 
   // получить команды из main
-   
   onCommand: (callback) =>
     ipcRenderer.on('overlay-command', (_, cmd) => callback(cmd)),
 
   // Попросить main изменить размер окна
-   
   resizeOverlay: (width, height) =>
     ipcRenderer.invoke('resize-overlay', { width, height }),
 
   // Включить или выключить игнорирование всех мышиных событий
-   
   setIgnoreMouseEvents: (ignore) =>
     ipcRenderer.invoke('overlay-set-ignore', ignore),
 });
